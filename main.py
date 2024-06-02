68d095ba5aae92f3ce776ce382b5828d73a1d330
TanThanhLe <tanthanhcr95@gmail.com>
2024-06-02 22:35:01 +0700

    Getter and setter

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ The following defines a Person class that has two attributes name and age, and c
 """
 
 john = Person("John", 18)
-print(john.age)
+# print(john.age)
 
 """
 Since age is the instance attribute of the Person class, you can assign it a new value like this:
 """
 
 john.age = 19
-print(john.age)
+# print(john.age)
 
 """
 The following assignment is also technically valid:
@@ -34,11 +34,56 @@ age = -1
 if age <= 0:
   raise ValueError("The age must be positive")
 else:
-  john.age = age
-
+  pass
+  # john.age = age
 """
 And you need to do this every time you want to assign a value to the age attribute.
 This is repetitive and difficult to maintain.
 
 To avoid this repetition, you can define a pair of methods called getter and setter.
+"""
+
+## Getter and setter
+
+"""
+The getter and setter methods provide an interface for accessing an instance attribute:
+
+- The getter returns the value of an attribute
+
+- The setter sets a new value for an attribute
+
+In our example, you can make the age attribute private (by convention) and define a getter and a setter to manipulate the age attribute.
+
+The following shows the new Person class with a getter and setter for the age attribute:
+"""
+
+"""
+How it works.
+
+In the Person class, the set_age() is the setter and the get_age() is the getter.
+By convention the getter and setter have the following name get_<attribute>() and set_<attribute>():
+
+In the set_age() method, we raise a ValueError is the age is less than or equal to zero.
+Otherwise, we assign the age argument to the _age attribute:
+
+The get_age() method returns the value of the _age attribute:
+
+In the __init__() method, we call the set_age() setter method to initialize the _age attribute:
+
+The following attempts to assign an invalid value to the age attribute
+"""
+
+john = Person("John", 18)
+john.set_age(-19)
+
+"""
+And Python issued a ValueError as expected.
+
+This code works just fine. But it has a backward compatibility issue.
+
+Suppose you released the Person class for a while and other developers have been already using it.
+
+And now you add the getter and setter, all the code that uses the Person won't work anymore.
+
+To define a getter and setter method while achieving backward compatibility, you can use the property() class.
 """
--- person.py
+++ person.py
@@ -1,4 +1,13 @@
 class Person:
   def __init__(self, name, age) -> None:
     self.name = name
-    self.age = age
+    self.set_age(age)
+
+  def set_age(self, age):
+    if age <= 0:
+      raise ValueError("The age must be positive")
+    else:
+      self._age = age
+
+  def get_age(self):
+    return self._age
